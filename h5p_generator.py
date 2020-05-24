9a297eff3e2a346234dc919b1a2d8fb8b81ec944
stevesl <stevesl@umich.edu>
2020-05-24 16:57:07 -0400

    Created sys for h5p template and scipt is able to import video and output combined video

--- h5p_generator.py
+++ h5p_generator.py
@@ -1,36 +1,60 @@
-import moviepy
+import os
+import glob
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
 
 # Input: Directory path where the videos are at.
-# Output: Create and return video objects.
-def importVideos(directoryPath):
+# Output: Creates and returns video objects.
+def importVideos(videosDirectoryPath, videoFileType):
 
-    pass
+    videoFileNames = glob.glob(
+        os.path.join(
+            videosDirectoryPath,
+            "*." + videoFileType
+        )
+    )
+    videoFileNames.sort()
+    videos = []
+    for videoFileName in videoFileNames:
+        video = VideoFileClip(videoFileName)
+        videos.append(video)
+    return videos
 
 
-# The issues that might come up would be the memory issue if the videos
-# are too large and to
-def combineVideos(videos):
-    pass
+def combineVideos(videos, outputDirectoryPath, outputVideoFileName):
+
+    finalVideo = concatenate_videoclips(videos)
+    finalVideo.write_videofile(
+        os.path.join(
+            outputDirectoryPath,
+            outputVideoFileName
+        )
+    )
 
 
 def main():
 
-    from moviepy.editor import VideoFileClip, concatenate_videoclips
-    clip1 = VideoFileClip("./tests/original.mov")
-    print(clip1.duration)
-    # clip2 = VideoFileClip("./tests/processed.mov").subclip(3, 7)
-    # final_clip = concatenate_videoclips([clip1, clip2])
-    # final_clip.write_videofile("./tests/my_concatenation.mp4")
-
     # Import videos
-    directoryPath = "./tests"
-    videos = importVideos(directoryPath)
+    videosDirectoryPath = "./test_contents/videos"
+    videoFileType = "mov"
+    videos = importVideos(
+        videosDirectoryPath,
+        videoFileType
+    )
 
     # Combine videos in to a single video.
-    combinedVideo = combineVideos(videos)
+    outputDirectoryPath = "./test_contents/outputs"
+    outputVideoFileName = "final_h5p_video.mp4" # Only mp4 is supported for now.
+    combineVideos(
+        videos,
+        outputDirectoryPath,
+        outputVideoFileName
+    )
 
     # Create content.json and h5p.json.
-
+    for video in videos:
+        print(video.filename)
+        print(video.duration)
 
 
 
